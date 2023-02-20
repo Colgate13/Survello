44edb385deaa83261f246f93c0c8140e06b70624
Gabriel Barros Feitosa Sá <gabreilbarros13@gmail.com>
2023-02-19 21:57:28 -0300

    Multi Process Infra

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,24 @@ import Server from './infra/http/server';
 import dotenv from 'dotenv';
 import Debug from 'debug';
 
-export function app() {
+import ProcessController from './infra/process/Controller';
+import cluster from 'cluster';
+
+export function app(multiProcess = false) {
   const debug = Debug('app:server');
   debug('Starting server...');
   dotenv.config();
 
   const server = new Server(process.env.PORT || 3380);
-  server.start();
+
+  if (cluster.isPrimary && multiProcess) {
+    ProcessController.PrimaryProcess();
+  } else {
+    ProcessController.SetNameWorker();
+    server.start();
+  }
 }
 
 if (process.argv[2] === 'start:app') {
-  app();
+  app(process.argv[3] === 'multi=true' ? true : false);
 }
