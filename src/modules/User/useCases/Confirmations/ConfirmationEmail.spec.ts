0d3516b0c049c41b4020c49c96fe76ebc66efaa3
Gabriel Barros Feitosa Sá <gabreilbarros13@gmail.com>
2023-04-27 20:54:46 -0300

    Update ConfirmationEmail.spec.ts

--- src/modules/User/useCases/Confirmations/ConfirmationEmail.spec.ts
+++ src/modules/User/useCases/Confirmations/ConfirmationEmail.spec.ts
@@ -2,6 +2,8 @@ import { ConfirmationEmail } from './ConfirmationEmail';
 import { InMemoryUsersRepository } from '../../repositories/InMemory/UsersRepository';
 import { IUsersRepository } from '../../repositories/IUsersRepository';
 
+import { JWT } from '../../Domain/jwt';
+
 import { CreateUser } from '../CreateUser/CreateUser';
 import { User } from '../../Domain/User';
 import { QueuMock } from '../../../../infra/Queue/RabbitQueueMock';
@@ -22,20 +24,6 @@ describe('Confirmation Email', () => {
   });
 
   it('should be able a edit user', async () => {
-    const newUser = await createUser.create({
-      email: 'gabrielbarros13@gmail.com',
-      password: '1515mOKC',
-      name: 'ColgAate13xx',
-    });
-
-    if (newUser.isLeft()) {
-      throw new Error('User not created');
-    }
-
-    expect(newUser.isRight()).toBe(true);
-    expect(newUser.isLeft()).toBe(false);
-    expect(newUser.value).toBeInstanceOf(User);
-
-    const user = await confirmationEmail.confirm(newUser.value);
+    //
   });
 });
